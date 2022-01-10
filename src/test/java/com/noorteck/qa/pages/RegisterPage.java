abc4bd58d64d652f24e722ee2508e0645c6eba25
agg72 <agg72@DESKTOP-BN6NDO2>
2022-01-10 16:49:35 -0500

    Fixing errors

--- src/test/java/com/noorteck/qa/pages/RegisterPage.java
+++ src/test/java/com/noorteck/qa/pages/RegisterPage.java
@@ -6,33 +6,34 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.noorteck.qa.utils.CommonUI;
 
-public class RegisterPage extends CommonUI {
+
+public class RegisterPage extends CommonUI{
 	
 	
 	@FindBy(name = "firstName")
-	WebElement firstNameField; 
-	
+	WebElement firstNameField;
+
 	@FindBy(name = "lastName")
 	WebElement lastNameField;
-	
+
 	@FindBy(name = "phone")
-	WebElement phoneNumberField; 
-	
+	WebElement phoneNumberField;
+
 	@FindBy(id = "userName")
 	WebElement emailField;
-	
+
 	@FindBy(name = "address1")
 	WebElement addressField;
-	
+
 	@FindBy(name = "city")
 	WebElement cityField;
-	
+
 	@FindBy(name = "state")
 	WebElement stateField;
-	
+
 	@FindBy(name = "postalCode")
 	WebElement postalCodeField;
-	
+
 	@FindBy(name = "country")
 	WebElement countryDropDown;
 	
@@ -44,76 +45,74 @@ public class RegisterPage extends CommonUI {
 	
 	@FindBy(name = "confirmPassword")
 	WebElement confirmPasswordField;
-	
+
 	@FindBy(name = "submit")
 	WebElement submitButton;
-	
+
 	@FindBy(xpath = "//*[@href='login.php']")
 	WebElement registerThankYouMessage;
-	
+
 	// page class constructor
 	public RegisterPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	// create methods for interaction
 	public void enterFirstName(String firstName) {
 		enter(firstNameField, firstName);
 	}
-	
+
 	public void enterLastName(String lastName) {
 		enter(lastNameField, lastName);
 	}
-	
+
 	public void enterPhoneNumber(String phoneNumber) {
 		enter(phoneNumberField, phoneNumber);
 	}
-	
+
 	public void enterEmail(String email) {
 		enter(emailField, email);
 	}
-	
+
 	public void enterAddress(String address) {
 		enter(addressField, address);
 	}
-	
+
 	public void enterCity(String city) {
 		enter(cityField, city);
-		
+
 	}
-	
+
 	public void enterState(String state) {
 		enter(stateField, state);
 	}
-	
+
 	public void enterZipCode(String zipcode) {
 		enter(postalCodeField, zipcode);
 	}
 	
 	public void selectCountry(String methodName, String indexTextValue ) {
 		selectFromDropdown(countryDropDown, methodName, indexTextValue);
-		
+
 	}
-	
+
 	public void enterUserName(String userName) {
 		enter(userNameField, userName);
 	}
-	
+
 	public void enterPassword(String password) {
 		enter(passwordField, password);
 	}
-	
+
 	public void enterConfirmPassword(String confirmPassword) {
-		enter(confirmPasswordField, confirmPassword );
+		enter(confirmPasswordField, confirmPassword);
 	}
-	
+
 	public void clickSubmitButton() {
 		click(submitButton);
 	}
-	
+
 	public void verifyRegister() {
 		getText(registerThankYouMessage);
 	}
-	
-
 }
